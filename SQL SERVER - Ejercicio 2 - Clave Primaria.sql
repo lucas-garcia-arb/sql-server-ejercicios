507bf72b49f0d81cf3dc7d4624d2f88d115008cc
Lucas García <lucasgarcia.developer@gmail.com>
2023-11-15 13:51:50 -0300

    Add primary key exercise 2

--- SQL SERVER - Ejercicio 2 - Clave Primaria.sql
+++ SQL SERVER - Ejercicio 2 - Clave Primaria.sql
@@ -1,52 +1,46 @@
+--1
+
 IF OBJECT_ID('alumnos') IS NOT NULL
 	DROP TABLE alumnos;
+GO
+
+--2
+CREATE TABLE alumnos(
+	legajo			VARCHAR(4) NOT NULL,
+	documento		VARCHAR(8),
+	nombre			VARCHAR(30),
+	domicilio		VARCHAR(30),
+	PRIMARY KEY(documento),
+	PRIMARY KEY(legajo)
+);
+--AGREGO SENTENCIA GO PARA PASAR EL ERROR Y SEGUIR EJECUTANDO
+GO
 
 CREATE TABLE alumnos(
 	legajo			VARCHAR(4) NOT NULL,
 	documento		VARCHAR(8),
 	nombre			VARCHAR(30),
 	domicilio		VARCHAR(30),
-	PRIMARY KEY (documento),
-	PRIMARY KEY (legajo)
-
-
+	PRIMARY KEY(documento),
 );
 
+--4
+EXEC sp_columns alumnos;
 
+--5
+INSERT INTO alumnos (legajo,documento,nombre,domicilio) VALUES
+('A233','22345345','Perez Mariana','Colon 234'),
+('A567','23545345','Morales Marcos','Avellaneda 348'); 
 
-/*
+--6
+INSERT INTO alumnos (legajo,documento,nombre,domicilio) VALUES
+('A567','23545345','Morales Marcos','Avellaneda 348'); 
+--AGREGO SENTENCIA GO PARA PASAR EL ERROR Y SEGUIR EJECUTANDO
+GO
 
+--7
+INSERT INTO alumnos (legajo,documento,nombre,domicilio) VALUES
+('A334',NULL,'Perez Mariana','Colon 234');
 
- 2- Cree la tabla con la siguiente estructura intentando establecer 2 campos como clave primaria, el
-campo "documento" y "legajo" (no lo permite):
- create table alumnos(
-legajo varchar(4) not null,
-documento varchar(8),
-nombre varchar(30),
-domicilio varchar(30),
-primary key(documento),
-primary key(legajo)
-); 
-
- 3- Cree la tabla estableciendo como clave primaria el campo "documento":
-create table alumnos(
-legajo varchar(4) not null,
-documento varchar(8),
-nombre varchar(30),
-domicilio varchar(30),
-primary key(documento)
-); 
-
- 4- Verifique que el campo "documento" no admite valores nulos:
-exec sp_columns alumnos; 
-
- 5- Ingrese los siguientes registros:
-insert into alumnos (legajo,documento,nombre,domicilio)
-values('A233','22345345','Perez Mariana','Colon 234');
- insert into alumnos (legajo,documento,nombre,domicilio)
-values('A567','23545345','Morales Marcos','Avellaneda 348'); 
-
- 6- Intente ingresar un alumno con número de documento existente (no lo permite) 
-
- 7- Intente ingresar un alumno con documento nulo (no lo permite)
-*/
+SELECT *
+FROM alumnos;
